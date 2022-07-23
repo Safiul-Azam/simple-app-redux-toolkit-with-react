c1f3dc9778f8405c4374b7231be16d7650ac8e7c
mdsafiulazam <mdriadkhan20@gmail.com>
2022-07-23 16:38:51 +0600

    install react-redux and redux-toolkit and create slice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CounterView from './features/counter/CounterView';
 function App() {
   return (
     <div className="App">
-      
+      <CounterView></CounterView>
     </div>
   );
 }
---
+++ src/features/counter/CounterView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CounterView = () => {
+    return (
+        <div>
+            <h2>counter Reducer</h2>
+        </div>
+    );
+};
+
+export default CounterView;
